39a897105aa9457ba4d416d381a2c37dd236da83
Monise Felzemburgh <monifelzem03@hotmail.com>
2023-08-16 17:05:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function addContact() {
   nameInput.name = "fullname";
   nameLi.appendChild(nameInput);
   ul.appendChild(nameLi);
+  ul.appendChild(document.createElement("br"));
 
   const phoneLi = document.createElement("li");
   phoneLi.innerText = "Telefone:";
@@ -21,6 +22,27 @@ function addContact() {
   phoneInput.name = "phone";
   phoneLi.appendChild(phoneInput);
   ul.appendChild(phoneLi);
+  ul.appendChild(document.createElement("br"));
+
+  const addressLi = document.createElement("li");
+  addressLi.innerHTML = '<label for="address"> Endereço: </label>';
+  const addressInput = document.createElement("input");
+  addressInput.type = "text";
+  addressInput.name = "address";
+  addressInput.id = "address";
+  addressLi.appendChild(addressInput);
+  ul.appendChild(addressLi);
+  ul.appendChild(document.createElement("br"));
 
   contactSection.append(h3, ul);
+}
+
+function removeContact() {
+  const contactSection = document.getElementById("contacts-list");
+
+  const titles = document.getElementsByTagName("h3");
+  const contacts = document.getElementsByTagName("ul");
+
+  contactSection.removeChild(titles[titles.length - 1]);
+  contactSection.removeChild(contacts[contacts.length - 1]);
 }
