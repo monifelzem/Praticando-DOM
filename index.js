10f12e397eb019cc1c890552e49432f58d997295
Monise Felzemburgh <monifelzem03@hotmail.com>
2023-08-16 15:58:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function addContact() {
+  const contactSection = document.getElementById("contacts-list");
+
+  const h3 = document.createElement("h3");
+  h3.innerText = "Contato";
+
+  const ul = document.createElement("ul");
+
+  const nameLi = document.createElement("li");
+  nameLi.innerText = "Nome:";
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "fullname";
+  nameLi.appendChild(nameInput);
+  ul.appendChild(nameLi);
+
+  const phoneLi = document.createElement("li");
+  phoneLi.innerText = "Telefone:";
+  const phoneInput = document.createElement("input");
+  phoneInput.type = "text";
+  phoneInput.name = "phone";
+  phoneLi.appendChild(phoneInput);
+  ul.appendChild(phoneLi);
+
+  contactSection.append(h3, ul);
+}
